b53b7e21c8a26f1be9ac4e5b9e1bc2c6e97c1f8c
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-12-18 16:43:36 -0700

    burger app

--- section08/18-burger-burger-builder-dynamic-add-custom-button-component/src/containers/BurgerBuilder/BurgerBuilder.js
+++ section08/18-burger-burger-builder-dynamic-add-custom-button-component/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,6 +90,14 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: true });
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -103,8 +111,11 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
