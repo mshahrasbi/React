c9b3b79a0724ff475e93c91ae1a7a711dcd893df
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2021-04-23 09:51:38 -0700

    work with webpack

---
+++ section22/04-work-webpack-load-images/src/App.js
@@ -0,0 +1,30 @@
+
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import Users from './containers/users';
+import asyncComponent from './higherOrderComponent/asyncComponent';
+
+
+const AsyncPizza = asyncComponent( () => {
+    return import('./containers/pizza.js');
+});
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <Link to="/">Users</Link> | 
+                    <Link to="/pizza">Pizza</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users} />
+                    <Route path="/pizza" component={AsyncPizza} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
